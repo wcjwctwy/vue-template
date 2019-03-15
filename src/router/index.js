e46f300dd609d0591620ab0793be1e9229b0c160
wangcongjun <wangcongjun@chezhibao.com>
2019-03-15 10:51:40 +0800

    添加history 和 baseURL

--- src/router/index.js
+++ src/router/index.js
@@ -6,5 +6,7 @@ import routes from './router.js'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
--- src/router/router.js
+++ src/router/router.js
@@ -14,6 +14,6 @@ export default [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component:
-      () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+      () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
   }
 ]
